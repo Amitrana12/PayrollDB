f823c80c732252e65f61ee858ec11d756cd04f41
amit <[amirana14325@gmail.com]>
2021-04-17 08:25:59 +0530

    [Amit Rana]Added::extend table add some new colum basic pay etc.

--- UC9-extendsemployepayroll.sql
+++ UC9-extendsemployepayroll.sql
@@ -1,8 +1,3 @@
-
-alter table employee_payroll add phone_number varchar(10);
-alter table employee_payroll add address varchar(50) not null default 'TBD';
-alter table employee_payroll add department varchar(20) not null default 'TBD';
-
 alter table employee_payroll add deduction money;
 alter table employee_payroll add taxable_pay money;
 alter table employee_payroll add income_tax money;
@@ -10,5 +5,4 @@ alter table employee_payroll add net_pay money;
 
 update employee_payroll set deduction = 0, taxable_pay = 0, net_pay = 0;
 update employee_payroll set income_tax = 0;
-
 select * from employee_payroll;
